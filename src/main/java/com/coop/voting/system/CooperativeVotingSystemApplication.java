fdeac4f94d5737ecba0f920f3d08775c83fa97f2
Wallace Rocha <wr.wallacerocha@gmail.com>
2025-01-10 08:33:47 -0300

    chore: initial project structure

---
+++ src/main/java/com/coop/voting/system/CooperativeVotingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.coop.voting.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativeVotingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativeVotingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coop/voting/system/CooperativeVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coop.voting.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativeVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
